25db920c90e4d754946c03b71ed1d6f46608fafb
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-08-16 21:34:58 -0300

    Lendo e escrevendo .txt com python

---
+++ Python3/Arquivos_e_modulos/leitura_de_arquivos.py
@@ -0,0 +1,12 @@
+# Criando um arquivo com conteúdo
+
+# -- com a função "open" vai abrir como arquivo "exemplo.txt" e será aberto para
+# escrita"r" (no caso seria o objeto que nomeei) e o .write diz quando ele deve ser escrito
+# e escrever no arquivo "Hello World"
+with open("exemplo.txt", "w") as arquivo:
+    arquivo.write("Hello World!!!")
+
+
+    with open("exemplo.txt", "r") as arquivo:
+        conteudo = arquivo.read()
+        print(conteudo)
