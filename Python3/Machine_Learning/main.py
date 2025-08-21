995fa93a7d9f2d3b0455ef54915d3d5ccf2dd085
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-08-21 06:14:23 -0300

    Machine_Learning

---
+++ Python3/Machine_Learning/main.py
@@ -0,0 +1,18 @@
+from sklearn import tree
+
+# - Aqui o "x" vai ser como se fossem perguntas e o "y" a checagem com as respostas
+
+# Dados: [nota_prova, nota_prova2]
+
+x = [[9, 10], [1,2], [5,6], [7,8], [2,1]],
+
+# Classes: 1 = aprovado / 0 = reprovado
+y = [1, 0, 1, 1, 0, 1]
+
+clf = tree.DecisionTreeClassifier
+
+# fit é o método para ensinar o modelo com base nos dados
+clf = clf.fit(x, y)
+
+# deve prever 1 ou 0
+print(clf.predict([[4, 5]]))
