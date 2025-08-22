e6cb2b8d6d8bb3bfbe4bc3d8f811fafbcc22506f
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-08-22 05:57:54 -0300

    atualizacao com plot_tree

--- Python3/Machine_Learning/main.py
+++ Python3/Machine_Learning/main.py
@@ -4,15 +4,24 @@ from sklearn import tree
 
 # Dados: [nota_prova, nota_prova2]
 
-x = [[9, 10], [1,2], [5,6], [7,8], [2,1]],
+x = [[9, 10], [1,2], [5,6], [7,8], [2,1], [3, 2]]
 
 # Classes: 1 = aprovado / 0 = reprovado
 y = [1, 0, 1, 1, 0, 1]
 
-clf = tree.DecisionTreeClassifier
+clf = tree.DecisionTreeClassifier()
 
 # fit é o método para ensinar o modelo com base nos dados
 clf = clf.fit(x, y)
 
 # deve prever 1 ou 0
 print(clf.predict([[4, 5]]))
+
+# ------------------------
+
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,5))
+plot_tree(clf, filled=True)
+plt.show()
